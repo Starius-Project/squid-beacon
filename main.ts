6991090e2bb56f445163400bf4848fa8c2fae180
Mickael <40719970+Starius-Project@users.noreply.github.com>
2021-09-27 18:13:00 +0200

    change power

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.showString("SPECIMEN BEACON")
 radio.setGroup(1)
-radio.setTransmitPower(1)
+radio.setTransmitPower(7)
 basic.forever(function () {
     radio.sendString("1")
     basic.showString("S")
